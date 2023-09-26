cb21eda00554f79553a6955f2070801bde0b1ccf
Moksh parashar <parasharm239@gmail.com>
2023-09-26 13:23:42 +0530

    first one

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline-offset-auto bg-slate-400 inline">
+      Hello world!
+    </h1>
+  );
+};
+
+export default App;
